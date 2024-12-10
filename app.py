3b73ba7ea6328df60aef7cc72fbbeec170bf11e0
Faizan-Khan-DC <ghazanferfaizanuddinkhan@dcmail.ca>
2024-12-10 02:59:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,6 @@ def hello():
     return "Hello from Faizan Khan ECS Container."
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000) 
+
+#UPDATED
